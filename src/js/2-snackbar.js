e7e1486fec340ca759058c9b060521a73bea9537
Kate <ket-tro@gmail.com>
2025-02-06 17:03:45 +0200

    add index file

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = +form.elements.delay.value;
+  const state = form.elements.state.value;
+
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  })
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
